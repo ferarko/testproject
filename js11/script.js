2bd1c920dc273d182a73e7bd49a22195ee913d51
Misha` <michail.kalaidov`>
2022-10-01 15:38:23 +0200

    `first`

---
+++ js11/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+alert(`Hello`);
+// const result = confirm(`Are you here?`);
+// // console.log(result);
+// const answer = prompt(`Age?`, `18`);
+
+// 
+// answers[0] = prompt(`Name?`, ``);
+// answers[1] = prompt(`Work?`, ``);
+// answers[2] = prompt(`Time?`, ``);
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+// document.write(answers);
+
+// const category = `toys`;
+// console.log(`https://someurl.com/${category}/5`);
+// const user = `Ivan`;
+// alert(`Privet, ${user}`);
+
+// console.log(`arr` + ` - object`);
+// console.log(4 + `5`);
+
+// let incr = 10, decr = 10;
+// // incr++;
+// // decr--;
+
+// console.log(incr);
+// console.log(decr);
+// console.log(2*4 != 8);
+
+// const isChecked = true, isClose = false;
+// console.log(isChecked || isClose);
+
